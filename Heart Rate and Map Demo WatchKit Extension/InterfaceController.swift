7b5df577ebad2c9c963d7ce49c4a9ae1d88b4152
Shane Ragusa <shane.ragusa@gmail.com>
2017-07-07 11:21:32 -0400

    Initial Commit

---
+++ Heart Rate and Map Demo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Heart Rate and Map Demo WatchKit Extension
+//
+//  Created by Shane Ragusa on 7/7/17.
+//  Copyright © 2017 Shane Ragusa. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Heart Rate and Map Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Heart Rate and Map Demo
+//
+//  Created by Shane Ragusa on 7/7/17.
+//  Copyright © 2017 Shane Ragusa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
